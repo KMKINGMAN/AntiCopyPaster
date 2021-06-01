5fd6033500eaa94208e2b8e6736f7f287cb6d2da
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-06-01 23:36:43 +0300

    Update server-info.js

--- command/CopyPaster/server-info.js
+++ command/CopyPaster/server-info.js
@@ -1,3 +1,36 @@
+  
+/*
+---------------------------------------
+|    All rights reserved to KIGNAMN    |
+| If there is any error, just visit the|
+|        MeCodes Discord Server        |
+|     Phone Number: +962792914245      |
+---------------------------------------
+______███████████████████████
+______█████████████████████████
+______███████████████████████__██
+____▒░██████████████████████____██
+___▒░░████████████████████▒▒____██
+__▒▒░░███████████████████▒░▒___██
+__▒░░░▒█████████████████▒░░▒__██
+_▒▒░░░░▒███████████████▒░░▒_██
+▒░▒░░░░░▒█████████████▒░░░▒█
+▒▒░▒▒░░░▒▒███████████▒░░▒▒
+▒▒░░▒▒░░░▒███████████▒▒░░
+▒░▒▒░░▒▒░░░░▒▒░░░░░░░░▒▒
+▒▒░░▒░░▒▒░░░░▒░░░░░░░▒▒
+▒▒▒░░▒░░▒▒░░░░░░░░░░▒▒
+_▒░▒░░░▒▒░░░░░░░░░░▒▒
+__▒░▒░░░▒░░░░░░░░░░▒▒
+___▒░▒▒░░░░░░░░░░░░▒▒
+____▒░▒▒░░░░░░░░░░░▒▒
+______▒▒░░░░░░░░░░░▒▒
+_______▒▒░░░░░░░░░░▒▒
+________▒▒░░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+*/
 const discord = require("discord.js");
 const me = require('../../mecodes-congig.json');
 const guilmodal = require('../../me-mongomodals/CopyPasters/Guild')
@@ -5,13 +38,13 @@ const devs = me.divs
 module.exports = {
   name: "server-info",
   category: "copypaster",
-  description: "لجلب معلومات عن السيرفر",
+  description: "لعرض معلومات و ادله عن سيرفر الكوبي بيسترز",
   run: async (client, kmsg, args, PREFIX) => {
     if(!args[0]) return msg.reply('**Please Insert Server ID**')
     let data = await guilmodal.findOne({guildID: args[0]})
     if(!data) return kmsg.reply('**This is not copypaster Server**')
     kmsg.channel.send(`
-    > **Server Name ${data.guildname}*
+    > **Server Name ${data.guildname}**
     > **ServerID: ${data.guildID}** 
     > **Server OwnerID: ${data.ownerID}**
     > **Discriptin : ${data.des} **
