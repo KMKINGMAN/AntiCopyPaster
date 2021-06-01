24d661864c5bb8b0c21049f17fb0a4ebf4d882bb
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-06-01 23:39:50 +0300

    Update devlopers.js

--- command/devloper/devlopers.js
+++ command/devloper/devlopers.js
@@ -5,12 +5,13 @@ const devs = me.divs
 module.exports = {
   name: "devlopers",
   category: "MeCodes",
-  description: "لجلب معلومات التواصل مع المطور",
+  description: "معلومات التواصل مع مطور البوت",
   run: async (client, kmsg, args, PREFIX) => {
       kmsg.channel.send(`
       > ** Devloper Name : [KINGMAN] **
       > ** Devloper Github : [https://github.com/MeKINGMAN] ** 
       > ** Devloper Phone Number : [+962792914245] ** 
+      > ** Support Discord Server : [ https://discord.gg/2wJqGg3KY8 ] ** 
       `)
      
 } 
