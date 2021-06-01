697162124bd196916e505135e04b2137ef368168
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-06-01 23:35:13 +0300

    Update remove-server.js

--- command/CopyPaster/remove-server.js
+++ command/CopyPaster/remove-server.js
@@ -1,3 +1,36 @@
+  
+/*
+---------------------------------------
+|    All rights reserved to KIGNAMN    |
+| If there is any error, just visit the|
+|        MeCodes Discord Server        |
+|     Phone Number: +962792914245      |
+---------------------------------------
+______███████████████████████
+______█████████████████████████
+______███████████████████████__██
+____▒░██████████████████████____██
+___▒░░████████████████████▒▒____██
+__▒▒░░███████████████████▒░▒___██
+__▒░░░▒█████████████████▒░░▒__██
+_▒▒░░░░▒███████████████▒░░▒_██
+▒░▒░░░░░▒█████████████▒░░░▒█
+▒▒░▒▒░░░▒▒███████████▒░░▒▒
+▒▒░░▒▒░░░▒███████████▒▒░░
+▒░▒▒░░▒▒░░░░▒▒░░░░░░░░▒▒
+▒▒░░▒░░▒▒░░░░▒░░░░░░░▒▒
+▒▒▒░░▒░░▒▒░░░░░░░░░░▒▒
+_▒░▒░░░▒▒░░░░░░░░░░▒▒
+__▒░▒░░░▒░░░░░░░░░░▒▒
+___▒░▒▒░░░░░░░░░░░░▒▒
+____▒░▒▒░░░░░░░░░░░▒▒
+______▒▒░░░░░░░░░░░▒▒
+_______▒▒░░░░░░░░░░▒▒
+________▒▒░░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+*/
 const guilmodal = require('../../me-mongomodals/CopyPasters/Guild')
 const discord = require("discord.js");
 const me = require('../../mecodes-congig.json');
@@ -5,7 +38,7 @@ const devs = me.divs
 module.exports = {
   name: "removeserver",
   category: "copypaster",
-  description: "لازالة سيرفر من القائمة",
+  description: "لازالة سيرفر كوبيبيستر من القائمة",
   run: async (client, kmsg, args, PREFIX) => {
     if(!args[0]) return kmsg.reply(`**Please Insert ServerID**`);
     let data = await guilmodal.findOneAndDelete({guildID: args[0]})
