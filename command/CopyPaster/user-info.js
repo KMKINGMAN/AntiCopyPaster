84ff9cf25b964fb5260e62a5c349872d4419e4eb
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-06-01 23:37:50 +0300

    Update user-info.js

--- command/CopyPaster/user-info.js
+++ command/CopyPaster/user-info.js
@@ -1,22 +1,36 @@
-/**
- *     userID: {
-        type: String,
-        required: true
-    },
-    des : {
-        type: String,
-        default: "",
-    },
-    ocode:{
-        type: String,
-    },
-    cpcode:{
-        type: String,
-    },
-    reprteduser:{
-        type: String,
-    }
- */
+  
+/*
+---------------------------------------
+|    All rights reserved to KIGNAMN    |
+| If there is any error, just visit the|
+|        MeCodes Discord Server        |
+|     Phone Number: +962792914245      |
+---------------------------------------
+______███████████████████████
+______█████████████████████████
+______███████████████████████__██
+____▒░██████████████████████____██
+___▒░░████████████████████▒▒____██
+__▒▒░░███████████████████▒░▒___██
+__▒░░░▒█████████████████▒░░▒__██
+_▒▒░░░░▒███████████████▒░░▒_██
+▒░▒░░░░░▒█████████████▒░░░▒█
+▒▒░▒▒░░░▒▒███████████▒░░▒▒
+▒▒░░▒▒░░░▒███████████▒▒░░
+▒░▒▒░░▒▒░░░░▒▒░░░░░░░░▒▒
+▒▒░░▒░░▒▒░░░░▒░░░░░░░▒▒
+▒▒▒░░▒░░▒▒░░░░░░░░░░▒▒
+_▒░▒░░░▒▒░░░░░░░░░░▒▒
+__▒░▒░░░▒░░░░░░░░░░▒▒
+___▒░▒▒░░░░░░░░░░░░▒▒
+____▒░▒▒░░░░░░░░░░░▒▒
+______▒▒░░░░░░░░░░░▒▒
+_______▒▒░░░░░░░░░░▒▒
+________▒▒░░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+_________▒▒░░░░░░░░▒▒
+*/
 const discord = require("discord.js");
 const me = require('../../mecodes-congig.json');
 const usermodal = require('../../me-mongomodals/CopyPasters/User')
@@ -24,7 +38,7 @@ const devs = me.divs
 module.exports = {
   name: "user-info",
   category: "copypaster",
-  description: "لجلب معلومات عن الشخص",
+  description: "لعرض معلومات عن الكوبي بيستر",
   run: async (client, kmsg, args, PREFIX) => {
     if(!args[0]) return kmsg.reply('**Please Insert User ID**')
     let data = await usermodal.findOne({userID: args[0]})
