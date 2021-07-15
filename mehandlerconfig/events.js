c534e7965e7201fa29bd03b58ca7cd0a9d070bff
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-07-15 23:50:41 +0300

    Fixed

--- mehandlerconfig/events.js
+++ mehandlerconfig/events.js
@@ -48,7 +48,6 @@ module.exports = (client) => {
         table.addRow(file, '✅');
         client.on(event.name, (...args) => event.execute(...args, client));
         }else{
-          client.on(event.name, (...args) => event.execute(...args, client));
           table.addRow(file, `❌  -> missing a event's name`);
           continue;
         }
